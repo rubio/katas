914a18fabae0684c01beee2ff9388e21d8bc5e83
Francisco Jesús Rubio Reales <rubioreales@gmail.com>
2016-11-15 17:27:56 +0100

    refactor to visitor pattern and added wook rule

--- FizzBuz_oc/fizzbuzz.py
+++ FizzBuz_oc/fizzbuzz.py
@@ -1,23 +1,58 @@
 #! /usr/bin/env python
 
-class FizzBuzz(object):
-
-    def isFizz(self, x):
-        return not (x % 3)
-
-    def isBuzz(self, x):
-        return not (x % 5)
+class BaseRule(object):
+    def handles(self, x):
+        pass
 
     def say(self, x):
-        if self.isFizz(x) and self.isBuzz(x):
-            return 'FizzBuzz'
-        elif self.isFizz(x):
-            return 'Fizz'
-        elif self.isBuzz(x):
-            return 'Buzz'
-        else:
-            return x
+        pass
 
-fb = FizzBuzz()
-for i in range(101):
+class NormalRule(BaseRule):
+    def handles(self, x):
+        return True
+
+    def say(self, x):
+        return x
+
+class FizzRule(BaseRule):
+    def handles(self, x):
+        return not x % 3
+
+    def say(self, x):
+        return 'Fizz'
+
+class BuzzRule(BaseRule):
+    def handles(self, x):
+        return not x % 5
+
+    def say(self, x):
+        return 'Buzz'
+
+class FizzBuzzRule(BaseRule):
+    def handles(self, x):
+        return not x % 3 and not x % 5
+
+    def say(self, x):
+        return 'FizzBuzz'
+
+class WookRule(BaseRule):
+    def handles(self, x):
+        return not x % 7
+
+    def say(self, x):
+        return 'Wook'
+
+
+class FizzBuzz(object):
+    def __init__(self, rules):
+        self.rules = rules
+
+    def say(self, x):
+        for r in self.rules:
+            if r.handles(x):
+                return r.say(x)
+
+
+fb = FizzBuzz([FizzBuzzRule(), FizzRule(), BuzzRule(), WookRule(),  NormalRule()])
+for i in range(11):
     print fb.say(i)
