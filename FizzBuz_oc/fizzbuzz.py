472d7d31fcb3ef4bf141c8f5ca051ae5a600482f
Francisco Jesús Rubio Reales <rubioreales@gmail.com>
2016-11-15 16:35:38 +0100

    first version of fizzbuzz kata

---
+++ FizzBuz_oc/fizzbuzz.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+for i in range(100):
+    is3 = not(i % 3)
+    is5 = not(i % 5)
+    if is3 and is5:
+        print 'FizzBuzz'
+        continue
+    if is3:
+        print 'Fizz'
+        continue
+    if is5:
+        print 'Buzz'
+        continue
+    print i
